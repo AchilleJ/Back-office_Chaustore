30c65f57184b06636d8532a9009261c585f17621
achillejouguelet <achille.jouguelet@gmail.com>
2019-09-23 15:02:37 +0200

    button add modify and delete arelinked to each file which correspond (as in plan.txt)

--- php/management.php
+++ php/management.php
@@ -10,14 +10,36 @@
 	<h2>Add, modify or delete:</h2>
 
 	<div class="forms">
-	    <form method="post" action="" id="add">
+	<?php if ($_POST["choice"] == "Brand" || $_POST["choice"] == "Color" || $_POST["choice"] == "Size" || $_POST["choice"] == "Category") { ?>
+	    <form method="post" action="add_other.php" id="add">
 	        <input name="choice" type="submit" value="Add">
 	    </form>
 
-	    <form method="post" action="" id="modify">
+	    <form method="post" action="modify_other.php" id="modify">
 	        <input name="choice" type="submit" value="Modify">
 	    </form>
-	    <form method="post" action="" id="delete">
+	<?php } ?>
+ 	<?php if ($_POST["choice"] == "Product") { ?>
+ 		<form method="post" action="add_product.php" id="add">
+	        <input name="choice" type="submit" value="Add">
+	    </form>
+
+	    <form method="post" action="modify_product.php" id="modify">
+	        <input name="choice" type="submit" value="Modify">
+	    </form>
+
+	<?php } ?>
+	<?php if ($_POST["choice"] == "Stock") { ?>
+		<form method="post" action="add_stock.php" id="add">
+	        <input name="choice" type="submit" value="Add">
+	    </form>
+
+	    <form method="post" action="modify_stock.php" id="modify">
+	        <input name="choice" type="submit" value="Modify">
+	    </form>	
+	<?php } ?>
+	
+	    <form method="post" action="delete.php" id="delete">
 	        <input name="choice" type="submit" value="Delete">
 	    </form>
 	</div>
