a28df140a95dff65f935bcaad2117c82759a35f7
achillejouguelet <achille.jouguelet@gmail.com>
2019-10-09 16:24:56 +0200

    created table , made the dump.sql and created the register form. For the rest of this afternoon, crypt the password and send a confirmation mail

---
+++ chaustore_v2/php/form_register.php
@@ -0,0 +1,93 @@
+<?php
+require_once('../../php/connectDB.php');?>
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Simplon - Formulaire d'inscription</title>
+</head>
+<body>
+    <h1>Register form</h1>
+
+    <form method="post" action="form_register.php">
+        <p>
+            <label for="firstname">Your firstname</label>
+            <input type="text" id="fisrtname" name="firstname" value="<?php if(!empty($_POST['firstname'])) echo $_POST['firstname'];?>">
+        </p>
+        <p>
+            <label for="lastname">Your lastname</label>
+            <input type="text" id="lastname" name="lastname" value="<?php if(!empty($_POST['lastname'])) echo $_POST['lastname'];?>">
+        </p>
+        <p>
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email" value="<?php if(!empty($_POST['email'])) echo $_POST['email'];?>">
+        </p>
+        <p>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" value="<?php if(!empty($_POST['password'])) echo $_POST['password'];?>">
+        </p>
+        <input type="submit" value="Create your account">
+    </form>
+    <?php
+
+    if(!empty($_POST)){
+        $msg = "";
+      
+        if(empty($_POST['firstname'])){
+            $msg .= "- Enter your firstname.<br/>";
+        }
+
+        if(empty($_POST['lastname'])){
+            $msg .= "- Enter your lastname.<br/>";
+        }
+
+        if(empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+            $msg .= "- Enter a correct email.<br/>";
+        }
+
+        if(empty($_POST['password'])){
+            $msg .= "- Enter your password.<br/>";
+        }
+
+        if(!empty($msg)){
+            $msg = "Erreur. Veuillez vérifier votre saisie :<br/>".$msg;
+            ?>
+            <p><?php echo $msg; ?></p>
+            <?php } 
+
+        else {
+
+            $sql = "INSERT INTO user (
+                    firstname, 
+                    lastname, 
+                    email)
+                    VALUES (
+                    '".$_POST['firstname']."', 
+                    '".$_POST['lastname']."', 
+                    '".$_POST['email']."'
+                    )";
+            
+        
+            $result = $conn->query($sql);
+
+            if($result){
+                $msg = "Thanks"." ".$_POST['firstname'].", Your account has been created";
+                ?> <p><?php echo $msg; ?></p> <?php
+            }
+
+            else if (mysqli_errno($conn) == 1062) {    //1062 = numero de l'erreur lié au doublon
+                $msg = "Email address already in use";
+                ?> <p><?php echo $msg; ?></p> <?php  
+            }
+
+            else {
+                echo 1;
+                $msg = "An error occured please try again later";
+                ?> <p><?php echo $msg; ?></p> <?php
+            }
+        }
+    }
+
+?>
+</body>
+</html>
